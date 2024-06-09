400ea438b75f8d7d2e7f36d665e17ebe5ae6f679
Kareem Salah <100665578+cln-Kafka@users.noreply.github.com>
2024-06-09 12:22:16 +0300

    Empty Files
    Included the paths of the header files and added the basic components of the simulation.

---
+++ Code/gpio/GPIO.h
@@ -0,0 +1,5 @@
+#ifndef GPIO_H
+#define GPIO_H
+void GPIO_Init(void);
+void GPIO_Write(unsigned char PinId, unsigned char PinData);
+#endif
---
+++ Code/src/main.c
@@ -0,0 +1,6 @@
+/*
+ * main.c
+ *
+ *  Created on: 9 Jun 2024
+ *      Author: Kareem Salah
+ */
