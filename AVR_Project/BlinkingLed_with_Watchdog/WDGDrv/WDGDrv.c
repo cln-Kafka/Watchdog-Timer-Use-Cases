61c31101100fd6dd1c0cfeeffb837b3c260851e5
Nadaaomran <104179154+Nadaaomran@users.noreply.github.com>
2024-06-20 16:51:08 +0300

    Added a timer with interrupt and modified the watchdog configuration

--- AVR_Project/BlinkingLed_with_Watchdog/WDGDrv/WDGDrv.c
+++ AVR_Project/BlinkingLed_with_Watchdog/WDGDrv/WDGDrv.c
@@ -1,19 +1,24 @@
 #include "WDGDrv.h"
 #include "WDGM.h"
-#define WDR()  __asm__ __volatile__ ("wdr");
 #include <avr/interrupt.h>
-#include <util/delay.h>
-#include "avr/wdt.h"
+#include <avr/wdt.h>
 /*Standard Types*/
 typedef unsigned long uint32;
 
-/*Registers*/
+/*Watchdog Registers*/
 /*
 #define SREG (*(uint32 *)(0x5F))
 #define WDTCSR (*(uint32 *)(0x60))
 #define MCUSR (*(uint32 *)(0x55))
 */
-/*Bit Positions*/
+/*Timer Registers*/
+/*
+#define TCCR1A (*(uint32 *)(0x80))
+#define TCCR1B (*(uint32 *)(0x81))
+#define OCR1A (*(uint32 *)(0x89))
+#define TIMSK1 (*(uint32 *)(0x6F))
+*/
+/*Watchdog Bit Positions*/
 #define PIN_I 7
 #define WDCE 4
 #define WDE 3
@@ -22,49 +27,55 @@ typedef unsigned long uint32;
 #define WDP0 0
 #define WDRF 3
 
+/*Timer1 Bit Positions*/
+#define WGM12 3
+#define CS11 1
+#define CS10 0
+#define OCIE1A 1
+
 extern char stuck_flag;
  void WDGDrv_Init(void){
-//	 /*Disable global interrupts*/
-//	 SREG &= ~(1<<PIN_I); //we can also use the macro "cli()" provided in the AVR header library
-//	 /*reset the timer*/
-//	 WDR(); //we can also use the wdt_reset() function provided in the AVR header library
-//	 // Clear the watchdog reset flag
-//	 MCUSR &= (~(1<< WDRF));
-//	 /*set the watchdog change enable and system reset enable to 1 in the same instruction before making any changes, page 44*/
-//	 WDTCSR |= (1<<WDCE)|(1<<WDE);
-//	 /*enable watchdog interrupt mode and system reset mode
-//	  * Set maximum time-out value = 8K cycles (~64 ms) by seting the watchdog timer prescale pins to 0010
-//	  */
-//	 WDTCSR = (1<<WDIE)| (1<<WDE)| (1<<WDP1);
-//	 /*enable global interrupts by setting pinI in the AVR status register to 1*/
-//     SREG |= (1<<PIN_I); //we can also use the macro "sei()" provided in the AVR header library
+	 /*Disable global interrupts*/
+	 SREG &= ~(1<<PIN_I);
+	 /*reset the timer*/
 	 wdt_reset();
-	 wdt_enable(WDTO_60MS);
-	 sei();
-	 WDTCSR |= (1<<WDIE);
+	 // Clear the watchdog reset flag
+	 MCUSR &= (~(1<< WDRF));
+	 /*set the watchdog change enable and system reset enable to 1 in the same instruction before making any changes, page 44*/
+	 WDTCSR |= (1<<WDCE)|(1<<WDE);
+	 /*enable system reset mode
+	  * Set maximum time-out value = 8K cycles (~64 ms) by seting the watchdog timer prescale pins to 0010
+	  */
+	 WDTCSR = (1<<WDE)| (1<<WDP1);
+	 /*Normal port operation, OC1A/OC1B disconnected
+	  * CTC mode*/
+	 TCCR1A = 0;
+	 /*CTC mode
+	  * 64ms prescale*/
+	 TCCR1B = (1 << WGM12) | (1 << CS11) | (1 << CS10);
+	 /* Set the compare value which achieves 50 ms timer
+	  * compare value (no of pulses needed to reach the timeout) = desired time/ time of one pulse
+	  * time of one pulse= prescale/ freq of the microcontroller clock
+	  * */
+	 OCR1A = 780;
+	 // Enable timer1 output compare A match interrupt
+	 TIMSK1 = (1 << OCIE1A);
+	 /*enable global interrupts by setting pinI in the AVR status register to 1*/
+     SREG |= (1<<PIN_I);
 
  }
-//
-// ISR(WDT_vect) {
-//	 WDGM_StatusType wdgmStatus = WDGM_PovideSuppervisionStatus();
-//	// Check if the WDGM status is OK
-//	if (wdgmStatus == OK)
-//    {	   //restart the timer without resetting the program
-//		WDGDrv_Init();
-//	}
-// }
-// ISR(WDT_vect){
-//	 WDGDrv_IsrNotification();
-//
-// }
+
+ ISR(TIMER1_COMPA_vect){
+	 WDGDrv_IsrNotification();
+
+ }
 
  void WDGDrv_IsrNotification(void){
 	 WDGM_StatusType wdgmStatus = WDGM_PovideSuppervisionStatus();
 	 // Check if the WDGM status is OK
 	 if (wdgmStatus == OK && (stuck_flag != 1))
 	  {
-		 //restart the timer without resetting the program
-		   WDGDrv_Init();
+		 //restart the timer
+		   wdt_reset();
 	    }
  }
- ISR(WDT_vect, ISR_ALIASOF(WDGDrv_IsrNotification));
